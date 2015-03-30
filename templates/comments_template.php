32b6bbb3ce605ffc36bbbddc4bb6fe7cd7e83579
Natallya <Natallya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 03:20:37 +0000

    SEO link
    git-svn-id: https://plugins.svn.wordpress.org/solidopinion-comments/trunk@1123299 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/comments_template.php
+++ templates/comments_template.php
@@ -5,5 +5,5 @@ if ($so_option && isset($so_option['so_shortname']) && ($so_option['so_shortname
 ?>
 <div class="so_comments" data-sitename="<?php echo $so_option['so_shortname']; ?>"></div>
 <script src="//api.solidopinion.com/widget/embed.js" async="async"></script>
-<noscript><a href="http://api.solidopinion.com/frontend/simple/?sitename=<?php echo $so_option['so_shortname']; ?>&thread_url=<?php echo $so_thread; ?>">Solidopinion Comments</a></noscript>
+<a href="http://api.solidopinion.com/seo/<?php echo $so_option['so_shortname']; ?><?php echo $_SERVER['REQUEST_URI']; ?>seo.html" style="font-size:10px;"><?php echo __('Сomments аrchive');?></a>
 <?php } ?>
