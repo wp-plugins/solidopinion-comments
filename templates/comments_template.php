340c4149b413e43167d76a478295663788ca0043
Natallya <Natallya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-04 21:59:24 +0000

    data-thread_url
    git-svn-id: https://plugins.svn.wordpress.org/solidopinion-comments/trunk@1174851 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/comments_template.php
+++ templates/comments_template.php
@@ -3,7 +3,7 @@ $so_option = get_option('so_options');
 $so_thread = str_replace(home_url(), '', get_permalink()); 
 if ($so_option && isset($so_option['so_shortname']) && ($so_option['so_shortname']!='')){
 ?>
-<div class="so_comments" data-sitename="<?php echo $so_option['so_shortname']; ?>" data-thread-url="<?php echo $so_thread; ?>"></div>
+<div class="so_comments" data-sitename="<?php echo $so_option['so_shortname']; ?>" data-thread_url="<?php echo $so_thread; ?>"></div>
 <script src="//api.solidopinion.com/widget/embed.js" async="async"></script>
 <a href="http://api.solidopinion.com/seo/<?php echo $so_option['so_shortname']; ?><?php echo $so_thread; ?>seo.html" style="font-size:10px;"><?php echo __('Сomments аrchive');?></a>
 <?php } ?>
